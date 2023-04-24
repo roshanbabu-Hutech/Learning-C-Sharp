55fc188bfc4dcee93bb9dcd7524a74aa201c3272
e <roshanbabu.kng@gmail.com>
2023-04-24 16:39:16 +0530

    19-4-2023-2
    all about methods

--- methods.cs
+++ methods.cs
@@ -19,3 +19,93 @@ namespace MyApplication
     }
   }
 }
+
+//method parameters
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod(string fname) 
+    {
+      Console.WriteLine(fname + " Refsnes");
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod("Liam");
+      MyMethod("Jenny");
+      MyMethod("Anja");
+    }  
+  }
+}
+
+// multiple parameters
+
+class Program
+  {
+    static void MyMethod(string fname, int age)
+    {
+      Console.WriteLine(fname + " is " + age);
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod("Liam", 5);
+      MyMethod("Jenny", 8);
+      MyMethod("Anja", 31);
+    }
+  }
+
+//   default parameter
+class Hello{
+static void MyMethod(string country = "Norway") 
+{
+  Console.WriteLine(country);
+}
+
+static void Main(string[] args)
+{
+  MyMethod("Sweden");
+  MyMethod("India");
+  MyMethod();
+  MyMethod("USA");
+}
+
+// Sweden
+// India
+// Norway
+// USA
+
+}
+
+// return values
+
+class Return{
+    static int MyMethod(int x, int y) 
+{
+  return x + y;
+}
+
+static void Main(string[] args)
+{
+  int z = MyMethod(5, 3);
+  Console.WriteLine(z);
+}
+
+// Outputs 8 (5 + 3)
+}
+
+class Names{
+    static void MyMethod(string child1, string child2, string child3) 
+{
+  Console.WriteLine("The youngest child is: " + child3);
+}
+
+static void Main(string[] args)
+{
+  MyMethod(child3: "John", child1: "Liam", child2: "Liam");
+}
+
+// The youngest child is: John
+}
