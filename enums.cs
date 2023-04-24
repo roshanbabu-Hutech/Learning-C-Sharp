ce19e86e350b41d718e5498152bf674e8846ee11
e <roshanbabu.kng@gmail.com>
2023-04-24 16:58:47 +0530

    24-4-2023-2
    enums

---
+++ enums.cs
@@ -0,0 +1,40 @@
+// Topic enums
+
+class Program
+{
+  enum Level
+  {
+    Low,
+    Medium,
+    High
+  }
+  static void Main(string[] args)
+  {
+    Level myVar = Level.Medium;
+    Console.WriteLine(myVar);
+  }
+}
+
+// using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    enum Months
+    {
+      January,    // 0
+      February,   // 1
+      March=6,    // 6
+      April,      // 7
+      May,        // 8
+      June,       // 9
+      July        // 10
+    }
+    static void Main(string[] args)
+    {
+      int myNum = (int) Months.April;
+      Console.WriteLine(myNum);
+    }
+  }
+}
