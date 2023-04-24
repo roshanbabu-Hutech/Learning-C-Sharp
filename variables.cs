65a1fa49c8de090cbbb2a67fe9b5a0f22cc0bdc5
e <roshanbabu.kng@gmail.com>
2023-04-24 15:55:19 +0530

    15-4-2023-3
    Type casting

--- variables.cs
+++ variables.cs
@@ -38,6 +38,23 @@ namespace HelloWorld
             int m = 60;
             Console.WriteLine(minutesPerHour);
             Console.WriteLine(m);
+
+
+            int myNum = 5;               // Integer (whole number)
+            double myDoubleNum = 5.99D;  // Floating point number
+            char myLetter = 'D';         // Character
+            bool myBool = true;          // Boolean
+            string myText = "Hello";     // String
+
+            // Type casting
+
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
         }
     }
 }
