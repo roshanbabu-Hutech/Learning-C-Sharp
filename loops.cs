86dc5c517f606a5f8f01f516fc8a1cdf7d8096be
e <roshanbabu.kng@gmail.com>
2023-04-24 16:21:10 +0530

    17-4-2023-2
    for loops

--- loops.cs
+++ loops.cs
@@ -22,5 +22,23 @@ namespace MyApplication
       }
       while (j < 5);
     }
+
+    // for loop
+    for(int k = 0; k <= 10; k = k + 2)
+      {
+        Console.WriteLine(k);
+      }
+    //   Nested loops
+    // Outer loop
+    for (int i = 1; i <= 2; ++i) 
+    {
+    Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+    // Inner loop
+    for (int j = 1; j <= 3; j++) 
+    {
+    Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
+  }
+}
     }
   }
