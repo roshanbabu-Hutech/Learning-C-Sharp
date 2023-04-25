5fe04bd1dce82edb72bfc4ebbdff25cf70d902ed
e <roshanbabu.kng@gmail.com>
2023-04-25 17:01:43 +0530

    25-04-2023
    connected to the ms sql server database , created tables and inserted the data into it

---
+++ WebApplication1/WebApplication1/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly database _db;
+        public CategoryController(database db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Data/database.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class database : DbContext
+    {
+        public database(DbContextOptions<database> options) : base(options)
+        {
+
+        }
+           public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Rakul Preet Singh", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Natasha Malkova", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Candy love", DisplayOrder = 3 }
+                );
+        }
+    }
+    }
+
