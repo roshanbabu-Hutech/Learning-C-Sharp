1ba0882129c90ee45c35b0261364c71020f7a74e
roshanbabu-Hutech <roshan.babu@hutechsolutions.com>
2023-04-26 17:37:33 +0530

    26-04-2023-4
    setup dotnet console app and sql server

--- WorkingOnModels/Program.cs
+++ WorkingOnModels/Program.cs
@@ -3,20 +3,50 @@
 
 using System;
 using Project.Models;
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Collections;
 
-namespace Project{
-    public class Program{
-        public static void Main(string[] args){
 
-            Computer myComputer = new Computer("Intel", 16, true, true, new DateTime(2000,01,01), 3000, "RTX 3050");
-            
-            Console.WriteLine(@$"My Computer has {myComputer.MotherBoard} motherboard and 
-            it's {myComputer.CpuCores} cores, 
-            also it has builtin {myComputer.HasWifi}, {myComputer.HasLte} and 
-            it's release date is {myComputer.ReleaseDate}
-            it's price is {myComputer.Price} finally it's a Nvidia {myComputer.VideoCard}");
-            // Computer.MotherBoard = "amd";
+namespace Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string connectionString = "server = DESKTOP-SEQKU0P; database = DotNetCourseDatabase; TrustServerCertificate = true; Trusted_Connection = true";
+
+            // Connecting...To the database
+
+            IDbConnection dbConnectionObject = new SqlConnection(connectionString);
+
+            string sqlQuery = "SELECT GETDATE()";
+
+            // after connection established from the database execute a query command on tables
+
+            DateTime rightNow = dbConnectionObject.QuerySingle<DateTime>(sqlQuery);
+
+            Console.WriteLine(rightNow);
+
+            Computer myComputer = new Computer("Intel", 16, true, true, new DateTime(2000, 01, 01), 3000, "RTX 3050");
+
+            // Console.WriteLine(@$"My Computer has {myComputer.MotherBoard} motherboard and 
+            // it's {myComputer.CpuCores} cores, 
+            // also it has builtin {myComputer.HasWifi}, {myComputer.HasLte} and 
+            // it's release date is {myComputer.ReleaseDate}
+            // it's price is {myComputer.Price} finally it's a Nvidia {myComputer.VideoCard}");
+            // // Computer.MotherBoard = "amd";
             // Console.WriteLine(Computer.MotherBoard);
+
+            int[] numbers = {10, 20, 30, 40, 50, 60};
+
+            IEnumerator numerator = numbers.GetEnumerator();
+
+            while(numerator.MoveNext()){
+                Console.WriteLine(numerator.Current);
+            }
         }
     }
 }
